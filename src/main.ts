a8ab180775db627530ac640a7bdcfc562849df89
farhodovbobur <farhodovbobur09@gmail.com>
2026-07-24 15:58:42 +0500

    MAJOR: Integrate global validation, API documentation with Swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { cleanupOpenApiDoc, ZodValidationPipe } from 'nestjs-zod';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ZodValidationPipe());
+  app.enableCors();
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('LIMONÉ API')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .build();
+  const document = cleanupOpenApiDoc(
+    SwaggerModule.createDocument(app, swaggerConfig),
+  );
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
