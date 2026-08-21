ef64826aa8f524f1f32004720a286dc21e4120c6
farhodovbobur <farhodovbobur09@gmail.com>
2026-08-21 17:54:27 +0500

    MINOR: Expand `CatalogModule` with `ProductBarcodesController`, service, entity, and related dependencies.

--- src/catalog/catalog.module.ts
+++ src/catalog/catalog.module.ts
@@ -3,12 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColorsController } from './colors.controller';
 import { ColorsService } from './colors.service';
 import { Color } from './entities/color.entity';
+import { ProductBarcode } from './entities/product-barcode.entity';
 import { ProductCategory } from './entities/product-category.entity';
 import { Product } from './entities/product.entity';
 import { ProductVariant } from './entities/product-variant.entity';
 import { Size } from './entities/size.entity';
 import { ProductCategoriesController } from './product-categories.controller';
 import { ProductCategoriesService } from './product-categories.service';
+import { ProductBarcodesController } from './product-barcodes.controller';
+import { ProductBarcodesService } from './product-barcodes.service';
 import { ProductVariantsController } from './product-variants.controller';
 import { ProductVariantsService } from './product-variants.service';
 import { ProductsController } from './products.controller';
@@ -24,6 +27,7 @@ import { SizesService } from './sizes.service';
       Color,
       Product,
       ProductVariant,
+      ProductBarcode,
     ]),
   ],
   controllers: [
@@ -32,6 +36,7 @@ import { SizesService } from './sizes.service';
     ColorsController,
     ProductsController,
     ProductVariantsController,
+    ProductBarcodesController,
   ],
   providers: [
     ProductCategoriesService,
@@ -39,6 +44,7 @@ import { SizesService } from './sizes.service';
     ColorsService,
     ProductsService,
     ProductVariantsService,
+    ProductBarcodesService,
   ],
   exports: [
     ProductCategoriesService,
@@ -46,6 +52,7 @@ import { SizesService } from './sizes.service';
     ColorsService,
     ProductsService,
     ProductVariantsService,
+    ProductBarcodesService,
   ],
 })
 export class CatalogModule {}
